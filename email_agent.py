569a88b57e946a9cabfb548369bed044c2332521
Heisenberg-617 <117323409+Heisenberg-617@users.noreply.github.com>
2026-01-17 19:42:57 +0100

    Add files via upload

---
+++ email_agent.py
@@ -0,0 +1,36 @@
+import os
+from typing import Dict
+
+import sendgrid
+from sendgrid.helpers.mail import Email, Mail, Content, To
+from agents import Agent, function_tool
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+GMAIL_FROM = os.getenv("GMAIL_FROM") # put your verified sender here
+GMAIL_TO = os.getenv("GMAIL_TO") # put your recipient here
+
+
+@function_tool
+def send_email(subject: str, html_body: str) -> Dict[str, str]:
+    """Send an email with the given subject and HTML body"""
+    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get("SENDGRID_API_KEY"))
+    from_email = Email(GMAIL_FROM)  # put your verified sender here
+    to_email = To(GMAIL_TO)  # put your recipient here
+    content = Content("text/html", html_body)
+    mail = Mail(from_email, to_email, subject, content).get()
+    response = sg.client.mail.send.post(request_body=mail)
+    print("Email response", response.status_code)
+    return "success"
+
+
+INSTRUCTIONS = """You are able to send a nicely formatted HTML email based on a detailed report.
+You will be provided with a detailed report. You should use your tool to send one email, providing the 
+report converted into clean, well presented HTML with an appropriate subject line."""
+
+email_agent = Agent(
+    name="Email agent",
+    instructions=INSTRUCTIONS,
+    tools=[send_email],
+    model="gpt-4o-mini",
+)
